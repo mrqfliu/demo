388edbd3377a0eca3470fa2866a73fadd723f195
qifan liu <165644714+mrqfliu@users.noreply.github.com>
2025-06-04 15:55:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 setup(
     name="demo",
     version="0.1.0",
-    install_requires=[  # 核心依赖
-        "sphinx_rtd_theme>=2.0.0",
-        "recommonmark",
-        "sphinx_markdown_tables"
-    ]
+    install_requires=[],  # 主依赖（根据实际情况填写）
+    extras_require={
+        "docs": [
+            "recommonmark",
+            "sphinx_markdown_tables",
+            "sphinx_rtd_theme>=2.0.0",
+        ]
 )
